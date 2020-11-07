59e204442d2abf53f2ba17af705b0765313ff52c
Ajay S <ajay_sieunarine@hotmail.com>
2020-11-07 00:10:56 -0400

    css/ start using material ui
    - refactor previous code to be styled with material ui

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './TodoList.css';
+import {List} from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 
 function TodoList(props) {
     return (
     <div className="TodoList">
-        <li>{props.taskName}</li>
+
+        <List className={"todo__list"}>
+            <ListItem>
+                <ListItemAvatar></ListItemAvatar>
+                <ListItemText primary={props.taskName} secondary={"Due: xx/xx/xx"} />
+            </ListItem>
+        </List>
+
     </div>
   )
 }
