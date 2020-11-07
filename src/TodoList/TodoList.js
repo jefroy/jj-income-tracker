186a7ec18acfb2a682954bfb8da58b1a4a3c3f43
Ajay S <ajay_sieunarine@hotmail.com>
2020-11-07 05:22:17 -0400

    implement edit modal and hook it up to firebase

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -9,10 +9,14 @@ import db from '../firebase';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Modal from "@material-ui/core/Modal";
 import EditIcon from '@material-ui/icons/Edit';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import firebase from "firebase";
 
 function TodoList(props) {
     // console.log('props: ', props);
+
     const [open, setOpen] = useState(false);
+    const [input, setInput] = useState('');
 
     function handleClose() {
         setOpen(false);
@@ -20,19 +24,53 @@ function TodoList(props) {
     function handleOpen() {
         setOpen(true);
     }
+    function updateToDo(){
+        // update taskname with new text
+        db.collection('todos').doc(props.id).set({
+            taskName: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }, {
+            merge: true // prevent overriding existing data
+        })
+        handleClose();
+    }
 
+    // styles hook https://www.positronx.io/create-react-modal-popup-with-material-ui/
+    const useStyles = makeStyles(theme => ({
+        modal: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        paper: {
+            position: 'absolute',
+            width: 450,
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }));
+
+    const classes = useStyles();
     return (
     <div className="TodoList">
 
         <> {/* <> is a react fragment */}
         <Modal
+            className={classes.paper}
             open={open} // state to determine if the modal is open or not
             onClose={handleClose} // function to handle closing the modal
             // children={}
         >
             <div>
-                <h1>i am modal</h1>
-                <button onClick={event => setOpen(false)}/>
+                <h1>Edit Task</h1>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={event => setInput(event.target.value)}
+                    placeholder={props.taskName}
+                />
+                <Button onClick={event => updateToDo()}>Update Task</Button>
             </div>
         </Modal>
 
